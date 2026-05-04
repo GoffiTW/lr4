da54b23ffed9392fc17c948994740a580b4b55fa
Greed <vashechko.mishas@gmail.com>
2026-05-04 21:22:48 +0300

    изменеия

---
+++ lr4/tools/GenerateDocs.ps1
@@ -0,0 +1,7 @@
+﻿# tools/GenerateDocs.ps1
+Set-Location $PSScriptRoot\..
+if (-not (Get-Command docfx -ErrorAction SilentlyContinue)) {
+    dotnet tool install -g docfx
+}
+docfx docs\docfx.json --serve
+Write-Host "Документация сгенерирована в docs\_site"
